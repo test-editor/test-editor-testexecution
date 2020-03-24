be92e2b18b28a2065cb70fb1b6f767aa8d2cdff9
Thorsten Hillebrand <thorsten.hillebrand@signal-iduna.de>
2020-03-24 17:07:36 +0100

    delete me later

--- shell.nix
+++ shell.nix
@@ -21,7 +21,7 @@ stdenv.mkDerivation {
         alias g="./gradlew"
         alias g.="../gradlew"
         export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 "
-        export JAVA_TOOL_OPTIONS="$_JAVA_OPTIONS -Djavax.net.ssl.trustStore=/etc/ssl/certs/ca-certificates.crt -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8"
+        export JAVA_TOOL_OPTIONS="$_JAVA_OPTIONS -Djavax.net.ssl.trustStore=/etc/ssl/certs/java/cacerts -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8"
         export LC_ALL=en_US.utf8
     '';
 }
