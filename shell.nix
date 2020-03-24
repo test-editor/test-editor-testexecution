5a6fd9b163f1ab894a92c415f224ec93cdcd8af8
Thorsten Hillebrand <thorsten.hillebrand@signal-iduna.de>
2020-03-24 14:58:50 +0100

    also write .javaTrustStore file

--- shell.nix
+++ shell.nix
@@ -23,6 +23,7 @@ stdenv.mkDerivation {
 
         export _TRUST_STORE=$(readlink -e $(dirname $(readlink -e $(which keytool)))/../lib/security/cacerts) ;
         export _JAVA_OPTIONS="-Djavax.net.ssl.trustStore=$_TRUST_STORE $_JAVA_OPTIONS "
+        echo "export _JAVA_OPTIONS=\"-Djavax.net.ssl.trustStore=$_TRUST_STORE \$_JAVA_OPTIONS\"" > .javaTrustStore
 
         export GRADLE_OPTS="$GRADLE_OPTS -Dorg.gradle.daemon=false -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 "
         export JAVA_TOOL_OPTIONS="$_JAVA_OPTIONS -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8"
